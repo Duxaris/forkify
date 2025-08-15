ecde71c8f0ad1093a93c1c4246af2a8c396769a3
duxaris <duxaris@outlook.com>
2025-08-15 13:04:40 +0100

    feat: Implement recipe search functionality and pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,28 +1,70 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import '../sass/main.scss';
-// const recipeContainer = document.querySelector('.recipe');
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
-
+    console.log(resultsView);
     // 1 loading recipe
     await model.loadRecipe(id);
 
     // Rendering Recipe
     recipeView.render(model.state.recipe);
+
+    // TEST
+
+    controlServings();
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1. get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. load search results
+    await model.loadSearchResults(query);
+
+    // 3. render results
+
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+    // 4 render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  // render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function () {
+  //update the recipe services (in state)
+  model.updateServings(8);
+  // Update the view
+  recipeView.render(model.state.recipe);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
